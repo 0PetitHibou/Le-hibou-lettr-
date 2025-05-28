c74ff0b07e5c52299e4d2aafd3de7c5821b257a3
Mickaël <mlagarde.pro@gmail.com>
2025-05-28 10:30:33 +0200

    feat(index): display book title in catalog

--- script.js
+++ script.js
@@ -2,17 +2,36 @@ async function fetchBook()
 {
     const API_URL = "https://openlibrary.org/search.json"
     const search = 'publish_year:[2020]';
-    const response = await fetch(`${API_URL}?q=${search}limit`);
+    const response = await fetch(`${API_URL}?q=${search}&limit=20`);
     const data = await response.json();
-    const books = data;
-    console.log(data.docs);
+    const books = data.docs;
+    console.log("FETCH BOOK: " , books);
     return books;
 }
 
 
-async function displayBook()
+function displayBook(books)
 {
-    display = document.querySelector("");
+    const display = document.querySelector(".catalog");
+    books.forEach(book => {
+        
+        const article = document.createElement("article");
+
+        const title = document.createElement("h2");
+        title.textContent = book.title;
+
+        article.append(title);
+        display.appendChild(article);
+    });
+
 }
 
-fetchBook();
+async function main()
+{
+    let books =await fetchBook();
+    console.log(books);
+    displayBook(books);
+}
+
+
+main();
