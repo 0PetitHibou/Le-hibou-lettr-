ed96e3a874553d03757be2a1c951bbd045e4d05a
Mickael Lagarde <mlagarde.pro@gmail.com>
2025-12-20 15:52:13 +0100

    (fix):login

--- asset/script/script.js
+++ asset/script/script.js
@@ -50,7 +50,6 @@ document.querySelector("#signupForm").addEventListener("submit", async function(
                 mail,
                 password
             })
-            console.log(body);
 
     try {
         const response = await fetch("http://localhost:3000/users", {
@@ -77,6 +76,44 @@ document.querySelector("#signupForm").addEventListener("submit", async function(
     }
 }
 )
+
+document.querySelector("#loginForm").addEventListener("submit", async function(e)
+{
+    e.preventDefault();
+    const mail = document.querySelector("#loginEmail").value.trim()
+    const password = document.querySelector("#loginPassword").value.trim()
+
+        const body = JSON.stringify({
+                mail,
+                password
+            })
+
+    try {
+        const response = await fetch("http://localhost:3000/login", {
+            method:"POST",
+            headers: {
+                "Content-Type" : "application/json"
+            },
+            body: body
+        });
+
+        const data = await response.json()
+
+        if (!response.ok) {
+            alert(data.error || "Utilisateur inconnu")
+            return;
+        }
+
+        localStorage.setItem("token", data.token);
+        localStorage.setItem("user", JSON.stringify(data.user));
+
+        console.log(data.user, "connecté")
+
+        window.location.href = "index.html";
+    } catch (error) {
+        console.log(error);
+    }
+})
 
 
 
